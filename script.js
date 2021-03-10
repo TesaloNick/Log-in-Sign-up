5d2df64a3f0ce818084c89d1ec28317190a6ce2b
Nikolay Antonovich <TesaloNick@gmail.com>
2021-03-10 21:49:15 +0300

    add wrong repeat

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const buttonSignUp = document.querySelector('.button-sign-up')
+const buttonSignUp = document.querySelector('.button-sign-up') // кнопки перехода
 const buttonLogIn = document.querySelector('.button-log-in')
 const buttonMainPage = document.querySelectorAll('.button-main-page')
-const logIn = document.querySelector('.log-in')
+
+const logIn = document.querySelector('.log-in')     // разделы сайта
 const signUp = document.querySelector('.sign-up')
 const mainPage = document.querySelector('.main-page')
 
-const adressRegistration = document.querySelector('.adress-registration')
+const adressRegistration = document.querySelector('.adress-registration')   // поля ввода данных
 const passwordRegistration = document.querySelector('.password-registration')
 const passwordRegistrationRepeat = document.querySelector('.password-registration-repeat')
 const adressLogIn = document.querySelector('.adress-log-in')
 const passwordLogIn = document.querySelector('.password-log-in')
 
-const sendRegistration = document.querySelector('.send-registration')
+const sendRegistration = document.querySelector('.send-registration')   // кнопки отправки данных
 const sendLogIn = document.querySelector('.send-log-in')
 
 let arrFormRegistration = []
@@ -21,7 +22,6 @@ const Site = function() {
         mainPage.style.display = 'block'
         signUp.style.display = 'none'
         logIn.style.display = 'none'
-        // const self=this
         buttonLogIn.addEventListener('click', self.showAuthPage)
         buttonSignUp.addEventListener('click', self.showRegistrationPage)
     }
@@ -29,27 +29,32 @@ const Site = function() {
         mainPage.style.display = 'none'
         signUp.style.display = 'block'
         logIn.style.display = 'none'
-        // const self=this
         buttonMainPage[1].addEventListener('click', self.showMainPage)
         sendRegistration.addEventListener('click', self.registration)
-        // debugger
-        // adressRegistration.value = ''
-        //     passwordRegistration.value = ''
-        //     passwordRegistrationRepeat.value = ''
-        buttonMainPage[1].removeEventListener('click', self.registration)
     }
     this.showAuthPage = function() {
         mainPage.style.display = 'none'
         signUp.style.display = 'none'
         logIn.style.display = 'block'
-        // const self=this
         buttonMainPage[0].addEventListener('click', self.showMainPage)
         sendLogIn.addEventListener('click', self.login)
-
     }
     this.registration = function() {
         const user = {email: adressRegistration.value, password: passwordRegistration.value, repeatPassword: passwordRegistrationRepeat.value}
-        arrFormRegistration.push(user)
+ 
+        if (passwordRegistration.value !== passwordRegistrationRepeat.value) {   
+                let wrongRepeatPassword = document.createElement('p')       // строка об ошибке повторного пароля
+                wrongRepeatPassword.innerHTML = 'Ва второй пароль не совпал с первым'
+                wrongRepeatPassword.style.color = 'red'
+                wrongRepeatPassword.classList.add('wrong-repeat-password')
+            if (!(signUp.contains(document.querySelector('.wrong-repeat-password')))) {
+                sendRegistration.insertAdjacentElement('beforebegin', wrongRepeatPassword)
+            } 
+        } else {
+            sendRegistration.previousElementSibling.remove()
+            arrFormRegistration.push(user)
+
+        }
         console.log(arrFormRegistration);
     }
     this.login = function() {
